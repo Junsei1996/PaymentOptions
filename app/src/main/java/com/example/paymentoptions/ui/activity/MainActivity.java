9405daf37243ad7f05eac57f36bc46dc17000523
Junsei1996 <athar.changezi@gmail.com>
2021-05-17 16:58:23 +0500

    Nav graph added to the project

--- app/src/main/java/com/example/paymentoptions/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/paymentoptions/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.paymentoptions;
+package com.example.paymentoptions.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.paymentoptions.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
